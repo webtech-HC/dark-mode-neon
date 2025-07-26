fac339ba4460bcc420e0a7126ca7939a680ffb0a
Heather C. <webtech.hc@gmail.com>
2025-07-25 20:16:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const testimonials = document.querySelectorAll('.testimonial');
+let index = 0;
+
+setInterval(() => {
+  testimonials[index].classList.remove('active');
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add('active');
+}, 4000);
